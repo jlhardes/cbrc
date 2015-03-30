1c770ed03f0a89fe02e9331ce4162dc7aa77e7fa
jhallida <jhallida@indiana.edu>
2015-03-30 13:07:15 -0400

    added missing 'class' attribute

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -40,6 +40,9 @@ class GenericFile < ActiveFedora::Base
   property :kingdom, predicate: ::RDF::Vocab::DWC.kingdom do |index|
 	index.as :stored_searchable, :facetable
   end
+  property :class, predicate: ::RDF::Vocab::DWC.class do |index|
+	index.as :stored_searchable, :facetable
+  end
   property :order, predicate: ::RDF::Vocab::DWC.order do |index|
 	index.as :stored_searchable, :facetable
   end
