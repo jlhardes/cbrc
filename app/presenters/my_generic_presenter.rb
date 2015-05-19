661ddd8fa24337a09ee0d35894b6d079c465cb22
jlhardes <jlhardes@iu.edu>
2015-05-19 11:26:28 -0400

    Update my_generic_presenter.rb
    Removing DWC terms and adding DWC terms per Gary's request.

--- app/presenters/my_generic_presenter.rb
+++ app/presenters/my_generic_presenter.rb
@@ -1,9 +1,9 @@
 class MyGenericPresenter < Sufia::GenericFilePresenter
   self.terms = [:resource_type, :title, :creator, :contributor, :description,
                        :tag, :rights, :publisher, :date_created, :subject, :language,
-                       :identifier, :based_near, :related_url, :basisOfRecord, :institutionCode, 
-                       :collectionCode, :catalogNumber, :occurrenceID, :country, :countryCode, 
-                       :stateProvince, :locality, :scientificName, :scientificNameAuthorship, 
-                       :taxonRank, :kingdom, :dwcclass, :order, :family, :genus, 
-                       :earlistPeriodOrLowestSystem, :earliestEpochOrLowestSeries, :formation]
+                       :identifier, :based_near, :related_url, 
+                       :catalogNumber, :otherCatalogNumbers, :continent, :country, :county, :higherGeography, 
+                       :stateProvince, :locality, :decimalLatitude, :decimalLongitude, :scientificName,
+                       :kingdom, :phylum, :dwcclass, :order, :family, :genus, :specificEpithet, 
+                       :infraspecificEpithet, :typeStatus, :associatedMedia]
 end
